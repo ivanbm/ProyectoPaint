d28cb60494f4bd50ea2a9d907757fb3adbff7eb0
Ivan <halcon931@gmail.com>
2015-02-05 20:20:58 +0100

    v1

---
+++ app/src/main/java/com/izv/android/proyectopaintfinal/MainActivity.java
@@ -0,0 +1,111 @@
+package com.izv.android.proyectopaintfinal;
+
+import java.util.UUID;
+
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private Vista vista;
+	private ImageButton colorActual, btBorrar, btNuevo, btGuardar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		vista = (Vista)findViewById(R.id.lienzo);
+
+		LinearLayout paintLayout = (LinearLayout)findViewById(R.id.colores);
+		colorActual = (ImageButton)paintLayout.getChildAt(0);
+
+		vista.settamPincel(10);
+
+		btBorrar = (ImageButton)findViewById(R.id.btBorrar);
+		btBorrar.setOnClickListener(this);
+
+		btNuevo = (ImageButton)findViewById(R.id.btNuevo);
+		btNuevo.setOnClickListener(this);
+
+		btGuardar = (ImageButton)findViewById(R.id.btGuardar);
+		btGuardar.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	public void colorSelect(View view){
+		vista.setborrar(false);
+		vista.settamPincel(10);
+
+		if(view!=colorActual){
+			ImageButton imgView = (ImageButton)view;
+			String color = view.getTag().toString();
+			vista.setColor(color);
+			colorActual=(ImageButton)view;
+		}
+	}
+
+	@Override
+	public void onClick(View view){
+
+		if(view.getId()==R.id.btNuevo){
+			AlertDialog.Builder nuevoDibu = new AlertDialog.Builder(this);
+			nuevoDibu.setTitle("Nuevo dibujo");
+			nuevoDibu.setMessage("Se perderán los cambios si no han sido guardados ¿Continuar?");
+			nuevoDibu.setPositiveButton("Si", new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog, int which){
+					vista.startNew();
+					dialog.dismiss();
+				}
+			});
+			nuevoDibu.setNegativeButton("Cancelar", new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog, int which){
+					dialog.cancel();
+				}
+			});
+			nuevoDibu.show();
+		}
+		else if(view.getId()==R.id.btGuardar){
+			AlertDialog.Builder guardar = new AlertDialog.Builder(this);
+			guardar.setTitle("Guardar");
+			guardar.setMessage("¿Desea guardar los cambios?");
+			guardar.setPositiveButton("Si", new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog, int which){
+					vista.setDrawingCacheEnabled(true);
+					MediaStore.Images.Media.insertImage(
+							getContentResolver(), vista.getDrawingCache(),
+							UUID.randomUUID().toString()+".png", "dibujo");
+					
+						Toast.makeText(getApplicationContext(), "Se ha guardado correctamente.", Toast.LENGTH_SHORT).show();
+					
+					vista.destroyDrawingCache();
+				}
+			});
+			guardar.setNegativeButton("No", new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog, int which){
+					dialog.cancel();
+				}
+			});
+			guardar.show();
+		}else if(view.getId()==R.id.btBorrar){
+            vista.setborrar(true);
+        }
+	}
+
+}
---
+++ app/src/main/java/com/izv/android/proyectopaintfinal/Vista.java
@@ -0,0 +1,117 @@
+package com.izv.android.proyectopaintfinal;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class Vista extends View {
+
+	private Path trazo;
+	private Paint pincel, miCanvas;
+	private int color = 0xFF660000;
+	private Canvas lienzo;
+	private Bitmap mapaDeBits;
+	private float tamPincel, lasttamPincel;
+	private boolean borrar=false;
+
+	public Vista(Context context, AttributeSet attrs){
+		super(context, attrs);
+		ajustesPincel();
+	}
+
+	private void ajustesPincel(){
+
+		tamPincel = getResources().getInteger(R.integer.medium_size);
+		lasttamPincel = tamPincel;
+		trazo = new Path();
+		pincel = new Paint();
+		pincel.setColor(color);
+		pincel.setAntiAlias(true);
+		pincel.setStrokeWidth(tamPincel);
+		pincel.setStyle(Paint.Style.STROKE);
+		pincel.setStrokeJoin(Paint.Join.ROUND);
+		pincel.setStrokeCap(Paint.Cap.ROUND);
+		miCanvas = new Paint(Paint.DITHER_FLAG);
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		mapaDeBits = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+		lienzo = new Canvas(mapaDeBits);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		canvas.drawBitmap(mapaDeBits, 0, 0, miCanvas);
+		canvas.drawPath(trazo, pincel);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		float touchX = event.getX();
+		float touchY = event.getY();
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			trazo.moveTo(touchX, touchY);
+			break;
+		case MotionEvent.ACTION_MOVE:
+			trazo.lineTo(touchX, touchY);
+			break;
+		case MotionEvent.ACTION_UP:
+			trazo.lineTo(touchX, touchY);
+			lienzo.drawPath(trazo, pincel);
+			trazo.reset();
+			break;
+		default:
+			return false;
+		}
+		invalidate();
+		return true;
+
+	}
+
+	public void setColor(String newColor){
+		invalidate();
+		color = Color.parseColor(newColor);
+		pincel.setColor(color);
+	}
+
+	public void settamPincel(float newSize){
+		float pixelAmount = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 
+				newSize, getResources().getDisplayMetrics());
+		tamPincel=pixelAmount;
+		pincel.setStrokeWidth(tamPincel);
+	}
+
+	public void setLasttamPincel(float lastSize){
+		lasttamPincel=lastSize;
+	}
+	public float getLasttamPincel(){
+		return lasttamPincel;
+	}
+
+	public void setborrar(boolean isborrar){
+		borrar=isborrar;
+		if(borrar){
+            this.setColor("#FFFFFFFF");
+            //pincel.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        }
+		else pincel.setXfermode(null);
+	}
+
+	public void startNew(){
+		lienzo.drawColor(0, PorterDuff.Mode.CLEAR);
+		invalidate();
+	}
+}
